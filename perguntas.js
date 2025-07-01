01b4bf747baa48dae8d1d2e6cf1879a91ec46367
Larissa Borgert <larissa.borgert@escola.pr.gov.br>
2025-07-01 16:53:15 -0300

    lalalalalalalalalalalalalalalalal

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'CIÊNCIAS'
+    'Como é feito a fotossintese?'
+    'Pelo SOL'
+)
+
+criaCartao(
+    'PROGRAMAÇÃO'
+    'O que é JavaScript?'
+    'É uma linguagem de programação!'
+)
+
+criaCartao(
+    'GEOGRAFIA'
+    'Qual a Capital do Brasil?'
+    'Brasília.'
+)
