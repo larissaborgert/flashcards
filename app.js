3dd8c0906fd30b2b2cc792b47ef331876af7613e
Larissa Borgert <larissa.borgert@escola.pr.gov.br>
2025-08-07 16:03:13 -0300

    lll

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 function criaCartao(categoria, pergunta, resposta) {
-   console.log(categoria,pergunta,resposta);
+   let container = document.getElementById('container');
+   let cartao = document.createElement('article');
+   cartao.className = 'cartao'
 
 }
+
+cartao.innerHTML
+<div class="cartao_conteudo">
+            <h3>QUÍMICA</h3>
+           <div class="cartao_conteudo_pergunta">
+            O que é um átomo?
+           </div>
+           <div class="cartao_conteudo_resposta">
+            Menor unidade de um elemento químico que ainda mantém suas propriedades químicas.
+           </div>
+           </div>
+           
